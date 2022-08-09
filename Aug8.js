3bf08f5aadb5a2721eeebe8639b681a17f9b73c0
maddybaum <madeline.baum94@gmail.com>
2022-08-08 20:21:40 -0500

    initial commit Aug 8

---
+++ Aug8.js
@@ -0,0 +1,6 @@
+/*
+Your task: Given a number of rings, return the MINIMUM number of moves needed to move all the rings from one tower to another.
+*/
+function towerOfHanoi(rings){
+    return 2**rings-1
+  }
